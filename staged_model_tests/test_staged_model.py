043fe83e8255efd4e2d92a2af29eac3446c35824
The-Laug <lauge.arn@gmail.com>
2026-01-16 11:08:56 +0100

    staged model fix

--- staged_model_tests/test_staged_model.py
+++ staged_model_tests/test_staged_model.py
@@ -1,5 +1,8 @@
-import wandb
+import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import wandb
 import time
 import torch
 from src.mlops_project.model import Model as MyModel
